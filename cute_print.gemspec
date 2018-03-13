bd4b3ced02f49282211e0b3e6c197d1f7921c7cf
Wayne Conrad <wconrad@hsmove.com>
2018-03-13 11:53:40 -0700

    No longer use hirb (closes #9)
    The hirb gem is no longer supported, and now causes warnings with
    ruby-2.4.0 or greater.  The warning is caused by a part of hirb we
    don't even use.  I extracted the one function we need from hirb and
    included it directly in cute_print.

--- cute_print.gemspec
+++ cute_print.gemspec
@@ -81,7 +81,6 @@ Gem::Specification.new do |s|
   s.add_development_dependency("rspec", "~> 3.1")
   s.add_development_dependency("rspec-given", "~> 3.5")
   s.add_development_dependency("yard", "~> 0.9.9")
-  s.add_runtime_dependency("hirb", "~> 0.7")
   s.add_runtime_dependency("ruby2ruby", "~> 2.1")
   s.add_runtime_dependency("ruby_parser", "~> 3.6")
 end
--- lib/cute_print/term_width/detected.rb
+++ lib/cute_print/term_width/detected.rb
@@ -1,5 +1,3 @@
-require "hirb"
-
 module CutePrint
   class TermWidth
 
@@ -24,7 +22,7 @@ module CutePrint
 
       def detect_width
         return width_override_for_tests if width_override_for_tests
-        width, _height = Hirb::Util.detect_terminal_size
+        width, _height = detect_terminal_size
         # Hirb returns nil if it can't determine the terminal width.
         # Program run in Emacs have an apparent terminal width of "0".
         width = FALLBACK_WIDTH if width.nil? || width == 0
@@ -34,6 +32,34 @@ module CutePrint
       def width_override_for_tests
         n = ENV['CUTE_PRINT_TERM_WIDTH']
         n && Integer(n)
+      end
+
+      # This function extracted from hirb version 0.7.3.  hirb is
+      # copyright (c) 2010 by Gabriel Horner and licensed under the
+      # MIT License.  This gem also uses the MIT License, so there is
+      # no problem with moving that function into here.
+      #
+      # The project: https://github.com/cldwalker/hirb
+      # The specific file:
+      # https://github.com/cldwalker/hirb/blob/master/lib/hirb/util.rb#L59
+      #
+      # We extracted the function because hirb is no longer maintained
+      # and now causes warnings with ruby-2.4.0 or greater.
+
+      # Returns [width, height] of terminal when detected, nil if not detected.
+      # Think of this as a simpler version of Highline's Highline::SystemExtensions.terminal_size()
+      def detect_terminal_size
+        if (ENV['COLUMNS'] =~ /^\d+$/) && (ENV['LINES'] =~ /^\d+$/)
+          [ENV['COLUMNS'].to_i, ENV['LINES'].to_i]
+        elsif (RUBY_PLATFORM =~ /java/ || (!STDIN.tty? && ENV['TERM'])) && command_exists?('tput')
+          [`tput cols`.to_i, `tput lines`.to_i]
+        elsif STDIN.tty? && command_exists?('stty')
+          `stty size`.scan(/\d+/).map { |s| s.to_i }.reverse
+        else
+          nil
+        end
+      rescue
+        nil
       end
 
     end
